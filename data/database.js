132612703731a5c6de6dff7db709c493e9f1dd3d
Abhishek Shankar <abhishek.201904069@tulas.edu.in>
2023-12-12 01:23:45 +0530

    final

--- data/database.js
+++ data/database.js
@@ -5,6 +5,6 @@ export const connectDB = () => {
 mongoose.connect(process.env.MONGO, {
     dbName: "backendapi",
 })
-    .then(() => console.log("connected db"))
+    .then((c) => console.log(`DB connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 }
